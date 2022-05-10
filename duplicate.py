7bf39f1b0ef82c0e98fda53ac4909c982c09373a
thevindu-w <thevinduwijesekera99@gmail.com>
2022-05-10 22:52:44 +0530

    performance improvement with sampling hash only if num of files > 2

--- duplicate.py
+++ duplicate.py
@@ -1,17 +1,17 @@
 # MIT License
 #
 # Copyright (c) 2022 H. Thevindu J. Wijesekera
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,9 +21,80 @@
 # SOFTWARE.
 
 
-
 from os import path, walk, remove
+from filecmp import cmp
 import hashlib
+from traceback import format_exc
+
+def compare_list(file_list):
+    if len(file_list) == 2:
+        if cmp(file_list[0], file_list[1], False):
+            yield file_list
+        return
+    hashes = {}
+    for file_name in file_list:
+        try:
+            sha256 = hashlib.sha256()
+            with open(file_name, 'rb') as file:
+                while True:
+                    buffer = file.read(4096)
+                    if not buffer:
+                        break
+                    sha256.update(buffer)
+                digest = sha256.digest()
+                if digest not in hashes:
+                    hashes[digest] = [file_name]
+                else:
+                    hashes[digest].append(file_name)
+        except: pass
+    for duplicates in hashes.values():
+        if len(duplicates)>1:
+            yield duplicates
+
+
+def sample_check(size, file_list, sample_cnt):
+    sample_map = {}
+    sample_arr = bytearray(sample_cnt)
+    step = size//sample_cnt
+    for file_name in file_list:
+        try:
+            with open(file_name, 'rb') as file:
+                for off in range(sample_cnt):
+                    file.seek(off*step, 0)
+                    buffer = file.read(1)
+                    if not buffer:
+                        break
+                    sample_arr[off] = buffer[0]
+                sample = bytes(sample_arr)
+                if sample not in sample_map:
+                    sample_map[sample] = [file_name]
+                else:
+                    sample_map[sample].append(file_name)
+        except: pass
+    for duplicates in sample_map.values():
+        if len(duplicates)>1:
+            yield duplicates
+
+
+def compare_file_list(size, file_list):
+    if size<=50:
+        for duplicate in compare_list(file_list):
+            yield duplicate
+        return
+    for l5 in sample_check(size, file_list, 5):
+        if size<5000:
+            for duplicate in compare_list(l5):
+                yield duplicate
+            continue
+        for l31 in sample_check(size, l5, 31):
+            if size<100000:
+                for duplicate in compare_list(l31):
+                    yield duplicate
+                continue
+            for l101 in sample_check(size, l31, 101):
+                for duplicate in compare_list(l101):
+                    yield duplicate
+
 
 def duplicate_remove(root, MIN_SIZE=-1):
     # dictionary with file size as key and list file names as value
@@ -56,34 +127,11 @@ def duplicate_remove(root, MIN_SIZE=-1):
         root_len = len(root) #length of root_dir name
         for size in sizes:
             l = size_dict.pop(size)
-            duplicates = dict()
-            for file_name in l:
-                try:
-                    sha256 = hashlib.sha256()
-                    with open(file_name, 'rb') as file:
-                        while True:
-                            buffer = file.read(4096)
-                            if not buffer:
-                                break
-                            sha256.update(buffer)
-                        digest = sha256.digest() # get SHA256 digest of the file
-                        if digest not in duplicates:
-                            duplicates[digest] = [file_name] # a list with one element
-                        else:
-                            duplicates[digest].append(file_name) # another file with same digest => duplicate
-                except: pass
-
-            #remove file names with no duplicates
-            keys = tuple(duplicates.keys())
-            for digest in keys:
-                if len(duplicates[digest]) == 1:
-                    duplicates.pop(digest)
 
             # iterate through duplicate lists and
             # prompt user to select which files to remove
-            for digest in duplicates:
+            for dup_list in compare_file_list(size, l):
                 print("Size = %i bytes"%size)
-                dup_list = duplicates[digest]
                 for i in range(len(dup_list)):
                     print("%i) %s" % (i+1, dup_list[i][root_len:])) # print 1 based indices
                 print()
@@ -100,4 +148,4 @@ def duplicate_remove(root, MIN_SIZE=-1):
         pass
 
 root = input("Enter path : ")
-duplicate_remove(root)
+duplicate_remove(root, 1)
