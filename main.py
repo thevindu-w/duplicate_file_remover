9b69b13708f1c7548ceb14b539f8106f02954f4e
thevindu-w <thevinduwijesekera99@gmail.com>
2023-07-21 17:59:59 +0530

    Modified the GUI

--- main.py
+++ main.py
@@ -78,8 +78,9 @@ home.geometry("%ix%i+%i+%i" % (int(screen_width*0.6),
               int(screen_height*0.6), int(screen_width*0.2), int(screen_height*0.2)))
 home.resizable(False, False)
 
-home.rowconfigure(0, minsize=25, weight=1)
+home.rowconfigure(0, minsize=30, weight=1)
 home.rowconfigure(1, weight=25)
+home.rowconfigure(2, minsize=30, weight=1)
 home.grid_columnconfigure(0, weight=1)
 home.grid_propagate(False)
 
@@ -88,6 +89,15 @@ top_frame = tk.Frame(home, bg="gray", height=25, pady=5)
 top_frame.grid(column=0, row=0, sticky=tk.NSEW)
 top_frame.grid_propagate(False)
 
+mid_frame = tk.Frame(home, bg="gray", height=25, padx=10, pady=5)
+mid_frame.grid(column=0, row=1, sticky=tk.NSEW)
+mid_frame.grid_propagate(False)
+
+bottom_frame = tk.Frame(home, bg="gray", height=25, pady=2)
+bottom_frame.grid(column=0, row=2, sticky=tk.NSEW)
+bottom_frame.grid_propagate(False)
+
+# Top frame
 top_frame.columnconfigure(0, weight=2)
 top_frame.columnconfigure(1, weight=30)
 top_frame.columnconfigure(2, weight=2)
@@ -106,35 +116,9 @@ scan_btn = tk.Button(top_frame, text="Scan", bg="green",
                      font="arial 13 bold", command=scan_btn_clk)
 scan_btn.grid(column=3, row=0, sticky=tk.EW, padx=5, pady=2)
 
-
-bottom_frame = tk.Frame(home, bg="gray")
-bottom_frame.grid(column=0, row=1, sticky=tk.NSEW)
-bottom_frame.grid_propagate(False)
-
-bottom_frame.columnconfigure(0, minsize=50, weight=1)
-bottom_frame.columnconfigure(1, weight=50)
-bottom_frame.columnconfigure(2, minsize=50, weight=1)
-
-bottom_frame.grid_rowconfigure(0, weight=1)
-
-
-back_btn = tk.Button(bottom_frame, text="<Back", command=back_btn_clk)
-back_btn.grid(column=0, row=0, sticky=tk.EW, padx=2, pady=2)
-back_btn.grid_propagate(False)
-
-next_btn = tk.Button(bottom_frame, text="Next>", command=next_btn_clk)
-next_btn.grid(column=2, row=0, sticky=tk.EW, padx=2, pady=2)
-next_btn.grid_propagate(False)
-
-
-mid_frame = tk.Frame(bottom_frame, bg="gray", pady=5)
-mid_frame.grid(column=1, row=0, sticky=tk.NSEW)
-mid_frame.grid_propagate(False)
-
-mid_frame.rowconfigure(0, weight=25)
-mid_frame.rowconfigure(1, minsize=30, weight=1)
-mid_frame.grid_columnconfigure(0, weight=1)
-# mid_frame.grid_propagate(False)
+# Mid frame
+mid_frame.columnconfigure(0, weight=1)
+mid_frame.grid_rowconfigure(0, weight=1)
 
 dup_container = tk.Frame(mid_frame, bg="white")
 dup_container.grid(column=0, row=0, sticky=tk.NSEW)
@@ -146,7 +130,7 @@ dup_container.grid_columnconfigure(0, weight=1)
 
 size_label = tk.Label(dup_container, bg="white",
                       anchor="w", font="arial 14 bold")
-size_label.grid(column=0, row=0, sticky=tk.NSEW, padx=5, pady=2)
+size_label.grid(column=0, row=0, sticky=tk.NSEW, padx=30, pady=2)
 size_label.grid_propagate(False)
 
 list_frame = tk.Frame(dup_container, bg="white")
@@ -172,21 +156,24 @@ dup_width = mid_frame.winfo_width() - scrollbar.winfo_width() - 1
 list_container = tk.Frame(canvas, width=dup_width)
 canvas.create_window((0,0), window=list_container, anchor='nw')
 
-btn_container = tk.Frame(mid_frame, bg="gray", pady=5)
-btn_container.grid(column=0, row=1, sticky=tk.NSEW)
-# btn_container.grid_propagate(False)
+# Bottom frame
+bottom_frame.columnconfigure(0, weight=2)
+bottom_frame.columnconfigure(1, weight=3)
+bottom_frame.columnconfigure(2, weight=2)
 
-btn_container.columnconfigure(0, weight=1)
-btn_container.columnconfigure(1, minsize=60)
-btn_container.columnconfigure(2, weight=1)
-btn_container.grid_propagate(False)
+bottom_frame.grid_rowconfigure(0, weight=1)
 
-tk.Frame(btn_container, bg="gray").grid(column=0, row=0, sticky=tk.NSEW)
-tk.Frame(btn_container, bg="gray").grid(column=2, row=0, sticky=tk.NSEW)
+back_btn = tk.Button(bottom_frame, text="<Back", command=back_btn_clk)
+back_btn.grid(column=0, row=0, sticky=tk.EW, padx=40, pady=2)
+back_btn.grid_propagate(False)
 
-del_btn = tk.Button(btn_container, text="Delete selected files",
+del_btn = tk.Button(bottom_frame, text="Delete selected files",
                     bg="red", font="arial 14 bold", command=del_btn_clk)
-del_btn.grid(column=1, row=0, sticky=tk.NSEW, padx=2, pady=2)
+del_btn.grid(column=1, row=0, sticky=tk.NSEW, padx=20, pady=2)
+
+next_btn = tk.Button(bottom_frame, text="Next>", command=next_btn_clk)
+next_btn.grid(column=2, row=0, sticky=tk.EW, padx=40, pady=2)
+next_btn.grid_propagate(False)
 
 list_container.update()
 
